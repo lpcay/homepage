301210a623fff6ed98ac37e20a25bd52dd819ead
fishcpy <2049879862@qq.com>
2025-09-17 22:48:01 +0800

    修改描述

--- app/app.config.ts
+++ app/app.config.ts
@@ -9,57 +9,17 @@ export default defineAppConfig({
 	...homepageConfig,
 
 	footer: {
-		copyright: `© ${new Date().getFullYear()} 纸鹿本鹿 (L33Z22L11)`,
+		copyright: `© ${new Date().getFullYear()} lpcay`,
 		message: h('a', { href: 'https://beian.miit.gov.cn/', target: '_blank', rel: 'noopener nofollow' }, '陕ICP备2025072742号-1'),
 	},
 
 	// 用于在主页展示下游引用
 	fork: [
 		{
-			img: 'https://image.m-c.top/?/images/2024/07/21/iyt3mhQCDe/b_a3f6e95501bcc4ce64c19c63a1211bcd.png',
-			link: 'https://oio.mckfs.com/',
-			text: 'Oiolosse MC服',
-		},
-		{
-			img: 'https://bu.dusays.com/2024/07/13/66929d307adb9.png',
-			link: 'https://henrywhu.cn/',
-			text: 'Henry Moreau',
-		},
-		{
-			img: 'https://api-space.tnxg.top/avatar?s=qq',
-			link: 'https://tnxg.top/',
-			text: '天翔TNXG',
-		},
-		{
-			img: 'https://ykc.im/icon.png',
-			link: 'https://ykc.im/',
-			text: 'York Chou',
-		},
-		{
-			img: 'https://www.xxfer.cn/icon.png',
-			link: 'https://www.xxfer.cn/',
-			text: '小李同学',
-		},
-		{
-			img: 'https://q1.qlogo.cn/g?b=qq&nk=1043865083&s=2',
-			link: 'https://www.xlenco.top/',
-			text: 'Xlenco',
-		},
-		{
-			img: 'https://www.mugzx.top/icon.png',
-			link: 'https://www.mugzx.top/',
-			text: 'Mugzx',
-		},
-		{
-			img: 'https://cn.cravatar.com/avatar/1F6C8947D35A8186A1647009BA8BC5F2?size=256',
-			link: 'https://www.kemiao.online/',
-			text: '克喵爱吃卤面',
-		},
-		{
-			img: 'https://q1.qlogo.cn/g?b=qq&nk=3310149631&s=2',
-			link: 'https://gr114.com/',
-			text: 'GreenRoc',
-		},
+			img: 'https://file.furrys.im/img/logo.webp',
+			link: 'https://www.furrys.im',
+			text: 'lpcay主页',
+		}
 	],
 
 	nav: [
@@ -67,9 +27,9 @@ export default defineAppConfig({
 			title: '',
 			items: [
 				{ icon: 'ph:house-duotone', text: '主页', url: '/' },
-				{ icon: 'ph:pen-nib-duotone', text: '文章', url: '/article' },
+				//{ icon: 'ph:pen-nib-duotone', text: '文章', url: '/article' },
 				// { icon: "ph:pen-nib-duotone", text: "文章", url: "https://blog.zhilu.cyou", external: true },
-				{ icon: 'ph:code-duotone', text: '项目', url: '/project' },
+				//{ icon: 'ph:code-duotone', text: '项目', url: '/project' },
 				{ icon: 'ph:globe-duotone', text: '站点', url: '/site' },
 				{ icon: 'ph:files-duotone', text: '日志', url: '/log' },
 			],
@@ -77,9 +37,7 @@ export default defineAppConfig({
 		{
 			title: '社交',
 			items: [
-				{ icon: 'basil:qq-outline', text: '群: 169994096', url: 'https://jq.qq.com/?_wv=1027&k=lQfNSeEd', external: true },
-				{ icon: 'ph:github-logo-duotone', text: 'Github', url: 'https://github.com/L33Z22L11', external: true },
-				{ icon: 'ph:telegram-logo-duotone', text: 'Telegram', url: 'https://t.me/L33Z22L11', external: true },
+				{ icon: 'ph:github-logo-duotone', text: 'Github', url: 'https://github.com/lpcay', external: true },
 			],
 		},
 	] satisfies Nav,
--- app/pages/home.vue
+++ app/pages/home.vue
@@ -23,47 +23,29 @@ const appConfig = useAppConfig()
 		<div>
 			<ZButton
 				icon="ph:files-duotone"
-				to="https://blog.zhilu.cyou/"
+				to="https://blog.furrys.im/"
 				text="博客"
 				primary
 			/>
 			<ZButton
 				icon="ph:github-logo-duotone"
-				to="https://github.com/L33Z22L11"
+				to="https://github.com/lpcay"
 				text="GitHub"
 			/>
 		</div>
 	</ZField>
 	<ZField label="介绍">
 		<p>
-			<ZLink to="https://github.com/xiyou-linuxer">西邮 Linux 兴趣小组</ZLink>
-			成员，Windows 11 和 Arch Linux 用户。
-		</p>
-	</ZField>
-	<ZField label="近期活动">
-		<p>
-			2024 年 7 月完善
-			<ZLink to="https://wiki.cooo.site/">西邮Wiki</ZLink>，8 月使用 Nuxt Content 重构了
-			<ZLink to="https://blog.zhilu.cyou/">个人博客</ZLink>，2025 年 3 月重构了西邮 Linux 兴趣小组博客后端，近期正在寻找前端秋招。
+			在平凡中点燃微光，照亮自己的远方。
 		</p>
 	</ZField>
 	<ZField label="关于主页">
 		<p>
-			自2024年6月1日开始重构，目前缓慢修缮中。
-		</p>
-		<p>
-			感谢<ZBadge link="https://github.com/KazariEX">
-				KazariEX
-			</ZBadge><ZBadge link="https://github.com/isYangs">
-				isYangs
-			</ZBadge>等众多朋友为个人主页提供帮助。
-			<br>
-			主页开源在
+			感谢纸鹿本鹿大佬的主页，开源在
 			<ZLink to="https://github.com/L33Z22L11/homepage-v5" icon="ph:github-logo-duotone">L33Z22L11/homepage-v5</ZLink>
 			上。
 		</p>
 		<p>
-			欢迎使用、参考样式：<br>
 			<ZBadge v-for="item in appConfig.fork" :key="item.link" v-bind="item" />
 		</p>
 	</ZField>
